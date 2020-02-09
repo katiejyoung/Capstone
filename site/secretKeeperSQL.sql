a1ba79ba37bd521f2f306dd3d774540b917807f2
dixonky <dixonky@oregonstate.edu>
2020-02-09 07:37:17 -0800

    Removed F and L, added comments

--- site/secretKeeperSQL.sql
+++ site/secretKeeperSQL.sql
@@ -1,7 +1,5 @@
 CREATE TABLE `user`(
 	`id` int NOT NULL AUTO_INCREMENT,
-    `user_first` varchar(30) NOT NULL,
-    `user_last` varchar(30) NOT NULL,
     `user_name` varchar(30) NOT NULL unique,
     `user_password` varchar(30) NOT NULL,
     `user_email` varchar(30) NOT NULL,
@@ -19,13 +17,13 @@ CREATE TABLE `records`(
     FOREIGN KEY (`user`) REFERENCES `user`(`id`) ON DELETE SET NULL
 );
 
-INSERT INTO `user`(`user_first`,`user_last`,`user_name`,`user_password`,`user_email`,`user_super`)
+INSERT INTO `user`(`user_name`,`user_password`,`user_email`,`user_super`)
 VALUES
-('Admin','Admin','Admin', 'password', 'admin@fake.com', 1),
-('Kyle','Dixon','Kyle', '12345', 'kdixon@fake.com' , 0),
-('Katie','Young','Katie', 'secret', 'kyoung@fake.com', 0),
-('John','Doe','John', '1q2w3e', 'jdoe@fake.com', 0),
-('Jane','Doe','Jane', 'password1', 'jdoe@fake.com', 0);
+('Admin', 'password', 'admin@fake.com', 1),
+('Kyle', '12345', 'kdixon@fake.com' , 0),
+('Katie', 'secret', 'kyoung@fake.com', 0),
+('John', '1q2w3e', 'jdoe@fake.com', 0),
+('Jane', 'password1', 'jdoe@fake.com', 0);
 
 INSERT INTO `records`(`record_name`, `record_data`, `record_URL`,`user`)
 VALUES
