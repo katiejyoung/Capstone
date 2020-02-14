d52cfcb5c7cc75b2b5149abb330a62e65d0af389
dixonky <46453847+dixonky@users.noreply.github.com>
2020-02-14 10:25:59 -0800

    Matched Master Code

--- site/secretKeeperSQL.sql
+++ site/secretKeeperSQL.sql
@@ -1,31 +1,32 @@
+DROP TABLE IF EXISTS `records`;
+DROP TABLE IF EXISTS `user`;
+
 CREATE TABLE `user`(
 	`id` int NOT NULL AUTO_INCREMENT,
-    `user_first` varchar(30) NOT NULL,
-    `user_last` varchar(30) NOT NULL,
-    `user_name` varchar(30) NOT NULL unique,
-    `user_password` varchar(30) NOT NULL,
-    `user_email` varchar(30) NOT NULL,
+    `user_name` varchar(50) NOT NULL unique,
+    `user_password` varchar(50) NOT NULL,
+    `user_email` varchar(50) NOT NULL,
     `user_super` int,
     PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `records`(
 	`record_id` int NOT NULL AUTO_INCREMENT,
-    `record_name` varchar(30) NOT NULL,
-    `record_data` varchar(30) NOT NULL,
-    `record_URL` varchar(30),
+    `record_name` varchar(50) NOT NULL,
+    `record_data` varchar(50) NOT NULL,
+    `record_URL` varchar(50),
     `user` int,
     PRIMARY KEY (`record_id`),
     FOREIGN KEY (`user`) REFERENCES `user`(`id`) ON DELETE SET NULL
 );
 
-INSERT INTO `user`(`user_first`,`user_last`,`user_name`,`user_password`,`user_email`,`user_super`)
+INSERT INTO `user`(`user_name`,`user_password`,`user_email`,`user_super`)
 VALUES
-('Admin','Admin','Admin', 'password', 'admin@fake.com', 1),
-('Kyle','Dixon','Kyle', '12345', 'kdixon@fake.com' , 0),
-('Katie','Young','Katie', 'secret', 'kyoung@fake.com', 0),
-('John','Doe','John', '1q2w3e', 'jdoe@fake.com', 0),
-('Jane','Doe','Jane', 'password1', 'jdoe@fake.com', 0);
+('Admin', 'password', 'admin@fake.com', 1),
+('Kyle', '12345', 'kdixon@fake.com' , 0),
+('Katie', 'secret', 'kyoung@fake.com', 0),
+('John', '1q2w3e', 'jdoe@fake.com', 0),
+('Jane', 'password1', 'jdoe@fake.com', 0);
 
 INSERT INTO `records`(`record_name`, `record_data`, `record_URL`,`user`)
 VALUES
